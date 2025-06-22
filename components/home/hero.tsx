f3a86fa0aa851825de0794692d7d18f779cff75b
Allaaxx <allanrao358@gmail.com>
2025-06-22 11:04:03 -0300

    fix: ajust scroll indicator position

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -113,7 +113,7 @@ export function Hero() {
       </div>
 
       {/* Simple scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 hidden md:block">
+      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 hidden md:block ">
         <div className="w-6 h-10 border-2 border-gray-300 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-fest-primary rounded-full mt-2 animate-pulse"></div>
         </div>
