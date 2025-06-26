7a17f7663e3ba3139cec54d96fa76f1f40e1a49c
Allaaxx <allanrao358@gmail.com>
2025-06-26 19:37:53 -0300

    feat: add test for jest render settings

--- tests/exemplo.test.tsx
+++ tests/exemplo.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('Exemplo de teste', () => {
   it('deve renderizar o texto corretamente', () => {
